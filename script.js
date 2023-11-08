41f392d9624b5ff9c6b122b6fceaebc13654a8a8
djordje-kojic <kojicdjordje@gmail.com>
2023-11-08 15:01:19 +0100

    dynamic indexing

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// FETCH LOCAL JSON FILE AND CALL DRAWTABLE FUNCTION
+ // FETCH LOCAL JSON FILE AND CALL DRAWTABLE FUNCTION
 fetch("data.json")
   .then((response) => response.json())
   .then((json) => drawTable(json));
@@ -6,7 +6,6 @@ fetch("data.json")
 //DRAWTABLE FUNCTION ----------------->
 function drawTable(data) {
   let table = document.getElementById("table");
-
   for (let i = 0; i < data.length; i++) {
     let row = `
         <tr>
@@ -22,16 +21,30 @@ function drawTable(data) {
 
 // ADDNEWW FUNCTION --------------->
 
-
 function addNew() {
   var name = document.getElementById("inputName").value;
   var age = document.getElementById("inputAge").value;
   var street = document.getElementById("inputStreet").value;
   var contry = document.getElementById("inputContry").value;
-  var id = 24;
+  var table = document.getElementById("table");
+
+  var totalRows = table.rows.length;
+  var id = totalRows;
 
   const newAdd = [
     {id:id, name: name, age:age, street: street, contry:contry}
   ];
+
   drawTable(newAdd);
+
+// add new entry in data.json
+
+  fetch("data.json")
+  .then((response) => response.json())
+  .then((json) => appendJSON(json, newAdd));
+}
+
+//APPEND JSON FILE WITH NEW ELEMENT
+function appendJSON (oldJSON, newAdd){
+    oldJSON.appendJSON(newAdd);
 }
